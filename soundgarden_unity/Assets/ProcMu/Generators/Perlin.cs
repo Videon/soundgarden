d2b6fc8df301de637c4338a99a54d88a4b7c5709
Vadim Nickel <bausn37@gmail.com>
2024-01-31 15:14:57 -0500

    Added Perlin algorithm and finished the first reflective journal entry
    I also simplified the music configuration UI, removing the possibility to set different intensity settings. In the original ProcMu, every music configuration had two intensity settings, one for lowest, one for highest intensity. The system could interpolate between the two, resulting in a modulation of the music's intensity. Removing this feature is only temporary until I figure out how to integrate affective features more closely with Russell's valence/arousal model while making the music system more modular overall, allowing for easier changes in the future.

--- soundgarden_unity/Assets/ProcMu/Generators/Perlin.cs
+++ soundgarden_unity/Assets/ProcMu/Generators/Perlin.cs
@@ -11,7 +11,9 @@ namespace ProcMu.Generators
     {
         public static List<Note> MakeNotes(List<Note> notesIn, MuConfig config, int index)
         {
-            List<Note> notesOut = notesIn;
+            int bottomNote = config.instrument[index].oct0.x * 12;
+            int topNote = config.instrument[index].oct0.y * 12 + 11;
+            int currentNote;
 
             float seed = Random.Range(-2048f, 2048f);
 
@@ -22,15 +24,34 @@ namespace ProcMu.Generators
                 float note = Mathf.PerlinNoise1D(seed + offset);
 
                 //Remap note to a value between the min and max octave.
-                note = math.remap(0f, 1f, 
-                        config.instrument[index].oct0.x * 12, config.instrument[index].oct0.y * 12, note);
+                note = math.remap(0f, 1f, bottomNote, topNote, note);
+
+                //Find closest note in scale
 
                 Debug.Log(note);
-                notesOut[i].note = (int)note;
+                notesIn[i].note = FindClosestNoteIndex((int)note, config.muScale.Scale);
             }
 
 
-            return notesOut;
+            return notesIn;
+        }
+
+        private static int FindClosestNoteIndex(int targetNote, bool[] scale)
+        {
+            // check from the target note upwards
+            for (int up = targetNote; up < scale.Length; up++)
+            {
+                if (scale[up]) return up;
+            }
+
+            // check from the target note downwards
+            for (int down = targetNote; down >= 0; down--)
+            {
+                if (scale[down]) return down;
+            }
+
+            // no active note found, return default value
+            return 0;
         }
     }
 }
