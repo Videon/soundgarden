9114ebfdf6db1dba308acfe5e46e8e78b4240fcc
Vadim Nickel <bausn37@gmail.com>
2024-02-17 01:48:33 -0500

    Removing Unity project
    Audio output with AudioHelm did not work in builds which is a dealbreaker. Hence it's back to Unreal where MetaSound seems to have all I need. I put together a basic setup, which after two days of work is already ahead of the Unity approach. Builds work as well. Therefore, I'm removing the Unity project. The next commit will push the Unreal project.
    Also, renaming the journal directory to docs for Github pages support in an attempt to make the journal experience nicer.

--- soundgarden_unity/Assets/ProcMu/UnityScripts/MusicConfiguration.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MC_", menuName = "Music/MusicConfiguration", order = 1)]
-public class MusicConfiguration : ScriptableObject
-{
-    public ScaleType scale;
-    public int rootNote;
-}
--- soundgarden_unity/Assets/ProcMu/UnityScripts/ScaleType.cs
+++
@@ -1,6 +0,0 @@
-public enum ScaleType
-{
-    Chromatic, Major, NaturalMinor, HarmonicMinor, MelodicMinorAscending,
-    PentatonicMajor, PentatonicMinor, Blues, WholeTone, Dorian, Phrygian,
-    Lydian, Mixolydian, Aeolian, Locrian
-}
